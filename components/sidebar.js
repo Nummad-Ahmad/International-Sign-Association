c6fcd64dd63294ef3298b612f6b1b9acf43bdd38
Nummad-Ahmad <116182616+Nummad-Ahmad@users.noreply.github.com>
2024-09-03 11:33:18 +0500

    Update sidebar.js

--- components/sidebar.js
+++ components/sidebar.js
@@ -1,26 +1,23 @@
 'use client';
 import Logo from '@/public/logo.png';
 import Image from 'next/image';
-import Calendar from '@/public/calendar.png';
-import Calendar2 from '@/public/calendar2.png';
 import Dashboard from '@/public/dashboard.png';
 import Dashboard2 from '@/public/dashboard2.png';
 import Events from '@/public/events.png';
 import Events2 from '@/public/events2.png';
-import Matchmaking from '@/public/matchmaking.png';
-import Matchmaking2 from '@/public/matchmaking2.png';
 import Meetings from '@/public/meetings.png';
 import Meetings2 from '@/public/meetings2.png';
 import Organizations from '@/public/organizations.png';
 import Organizations2 from '@/public/organizations2.png';
-import Subscription from '@/public/subscription.png';
-import Subscription2 from '@/public/subscription2.png';
 import Users from '@/public/users.png';
 import Users2 from '@/public/users2.png';
 import style from './sidebar.module.css';
 import { CiLogout } from "react-icons/ci";
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
+import { IoMdMenu } from "react-icons/io";
+import { IoClose } from "react-icons/io5";
+
 export default function Sidebar() {
     const [activeIndex, setSelectedIndex] = useState(() => {
         if (typeof window !== 'undefined') {
@@ -58,37 +55,68 @@ export default function Sidebar() {
             router.push('/')
         }
     }
+    const [deviceType, setDeviceType] = useState(false);
+    const [open, setOpen] = useState(false);
+    function toggleMobileSideBar() {
+        setOpen(!open);
+    }
     return (
-        <div className={style.sidebar}>
-            <div>
-                <Image className={style.logo} src={Logo} width={150} alt='' />
-            </div>
-            <div className={style.optionsDiv}>
-                <div onClick={() => changeOption(0)} className={`${style.option} ${activeIndex == 0 ? style.activeOption : ''}`}>
-                    <Image className={style.optionImage} src={activeIndex == 0 ? Dashboard2 : Dashboard} alt=''/>
-                    <p>Dashboard</p>
-                </div>
-                <div onClick={() => changeOption(1)} className={`${style.option} ${activeIndex == 1 ? style.activeOption : ''}`}>
-                    <Image className={style.optionImage} src={activeIndex == 1 ? Organizations2 : Organizations}  alt=''/>
-                    <p>Organizations</p>
-                </div>
-                <div onClick={() => changeOption(2)} className={`${style.option} ${activeIndex == 2 ? style.activeOption : ''}`}>
-                    <Image className={style.optionImage} src={activeIndex == 2 ? Events2 : Events}  alt=''/>
-                    <p>Events</p>
-                </div>
-                <div onClick={() => changeOption(3)} className={`${style.option} ${activeIndex == 3 ? style.activeOption : ''}`}>
-                    <Image className={style.optionImage} src={activeIndex == 3 ? Meetings2 : Meetings}  alt=''/>
-                    <p>Meetings schedule</p>
-                </div>
-                <div onClick={() => changeOption(4)} className={`${style.option} ${activeIndex == 4 ? style.activeOption : ''}`}>
-                    <Image className={style.optionImage} src={activeIndex == 4 ? Users2 : Users}  alt=''/>
-                    <p>Users</p>
-                </div>
-                <div className={style.logoutDiv} onClick={() => changeOption(5)}>
-                    <CiLogout color='rgb(100, 120, 120)' size={17}></CiLogout>
-                    <p>Logout</p>
-                </div>
-            </div>
-        </div>
+        <>
+            {
+                deviceType ?
+                    <div className={style.sidebar}>
+                        <div>
+                            <Image className={style.logo} src={Logo} width={150} alt='' />
+                        </div>
+                        <div className={style.optionsDiv}>
+                            <div onClick={() => changeOption(0)} className={`${style.option} ${activeIndex == 0 ? style.activeOption : ''}`}>
+                                <Image className={style.optionImage} src={activeIndex == 0 ? Dashboard2 : Dashboard} alt='' />
+                                <p>Dashboard</p>
+                            </div>
+                            <div onClick={() => changeOption(1)} className={`${style.option} ${activeIndex == 1 ? style.activeOption : ''}`}>
+                                <Image className={style.optionImage} src={activeIndex == 1 ? Organizations2 : Organizations} alt='' />
+                                <p>Organizations</p>
+                            </div>
+                            <div onClick={() => changeOption(2)} className={`${style.option} ${activeIndex == 2 ? style.activeOption : ''}`}>
+                                <Image className={style.optionImage} src={activeIndex == 2 ? Events2 : Events} alt='' />
+                                <p>Events</p>
+                            </div>
+                            <div onClick={() => changeOption(3)} className={`${style.option} ${activeIndex == 3 ? style.activeOption : ''}`}>
+                                <Image className={style.optionImage} src={activeIndex == 3 ? Meetings2 : Meetings} alt='' />
+                                <p>Meetings schedule</p>
+                            </div>
+                            <div onClick={() => changeOption(4)} className={`${style.option} ${activeIndex == 4 ? style.activeOption : ''}`}>
+                                <Image className={style.optionImage} src={activeIndex == 4 ? Users2 : Users} alt='' />
+                                <p>Users</p>
+                            </div>
+                            <div className={style.logoutDiv} onClick={() => changeOption(5)}>
+                                <CiLogout color='rgb(100, 120, 120)' size={17}></CiLogout>
+                                <p>Logout</p>
+                            </div>
+                        </div>
+                    </div> :
+                    <div className={style.mobileTopBar}>
+                        <Image src={Logo} alt='' height={90}></Image>
+                        <>
+                        {
+                            open &&
+                            <div className={style.mobileSideBar}>
+                                <span onClick={toggleMobileSideBar}>
+                                    <IoClose size={30} />
+                                </span>
+                                <div>
+                                    <div className={style.mobileSideBarOption}>Dashboard</div>
+                                    <div className={style.mobileSideBarOption}>Meetings</div>
+                                    <div className={style.mobileSideBarOption}>Organizations</div>
+                                </div>
+                            </div>
+                        }
+                            <span onClick={toggleMobileSideBar}>
+                                <IoMdMenu size={30} />
+                            </span> 
+                        </>
+                    </div>
+            }
+        </>
     );
 }
